c1ac42d687ceef851245a584e68216019478fc3f
Bruno Bonatti <bonattibrunodm@gmail.com>
2022-03-29 14:59:15 -0300

    parser

--- nodeParser/index.js
+++ nodeParser/index.js
@@ -1,33 +1,29 @@
-const csv = require('csv-parser');
-const fs = require('fs');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const csvWriter = createCsvWriter({
-    path: 'output.csv',
-    header: [
-        {id: 'fullname', title: 'fullname'}
-    ]
-})
-
-var _ = require('lodash');
-// Load the core build.
-var _ = require('lodash/core');
-// Load the FP build for immutable auto-curried iteratee-first data-last methods.
-var fp = require('lodash/fp');
-
-
-
-
+const csv = require('csv-parser')
+const fs = require('fs')
 const results = [];
-
-
+let _ = require('lodash');
 
 
 
 fs.createReadStream('input.csv')
     .pipe(csv({}))
     .on('data', (data) => results.push(data))
-    .on('end', async () => {
-       _.forEach(results, function(o){
-           console.log(o.fullname)
-       })
-    });
+    .on('end', () => {
+
+          var newArray=[];
+
+            results.forEach(obj => {
+                 newArray.push(obj)
+            })
+
+            console.log(_.merge(results, newArray))
+
+          
+     
+   
+         data = JSON.stringify(results);
+        
+    fs.writeFileSync('output.json', data);
+    })
+
+  
