3b8a6d5d7b6a821cf7fada4b2b443388cf489e1b
Bruno Bonatti <bonattibrunodm@gmail.com>
2022-04-20 02:16:08 -0300

    netflix

--- newNetflix/pages/mainpage.js
+++ newNetflix/pages/mainpage.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, SafeAreaView, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { useState, useEffect } from 'react';
 import api from '../services/api'
 import Carousel from 'react-native-snap-carousel';
@@ -7,6 +7,11 @@ import { useNavigation } from '@react-navigation/native';
 
 export default function App() {
   const [movies, setMovies] = useState([])
+  const [dcMovies, setDcMovies] = useState([])
+  const [american, setAmerican] = useState([])
+  const [copa, setCopa] = useState([])
+  const [hangover, setHangover] = useState([])
+
   const navigation = useNavigation();
 
   function navigateDetails(movieName){
@@ -21,6 +26,39 @@ export default function App() {
     })
   }, [])
 
+  useEffect(async () => {
+    const url = 'batman'
+    await api.get(url)
+    .then(response => {
+      setDcMovies(response.data.Search)
+    })
+  }, [])
+
+  useEffect(async () => {
+    const url = 'american'
+    await api.get(url)
+    .then(response => {
+      setAmerican(response.data.Search)
+    })
+  }, [])
+
+  useEffect(async () => {
+    const url = 'copa'
+    await api.get(url)
+    .then(response => {
+      setCopa(response.data.Search)
+    })
+  }, [])
+
+  useEffect(async () => {
+    const url = 'hangover'
+    await api.get(url)
+    .then(response => {
+      setHangover(response.data.Search)
+    })
+  }, [])
+
+
   function _renderItem({item,index}){
     return (
       <TouchableOpacity onPress={() => navigateDetails(item.Title)}>
@@ -28,7 +66,6 @@ export default function App() {
           backgroundColor:'#131313',
           borderRadius: 5,
           height: 250,
-          padding: 50,
           marginLeft: 25,
           marginRight: 25,
           alignItems: 'center',
@@ -43,8 +80,9 @@ export default function App() {
 }
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor:'#fff', paddingTop: 250, alignItems: 'center' }}>
-      <Text style={{fontSize: 30, fontWeight: 'bold', paddingBottom: 30}}>Choose your movie</Text>
+    <SafeAreaView style={{flex: 1, backgroundColor:'#131313', paddingTop: 70 }}>
+    <ScrollView>
+      <Text style={{fontSize: 20, fontWeight: 'bold', paddingBottom: 30, color: '#FFF', marginLeft: 30, marginTop: 20}}>Marvel</Text>
     <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center' }}>
         <Carousel
           layout={"default"}
@@ -54,6 +92,47 @@ export default function App() {
           renderItem={_renderItem}
            />
     </View>
+    <Text style={{fontSize: 20, fontWeight: 'bold', paddingBottom: 30, color: '#FFF', marginLeft: 30,  marginTop: 20}}>DC</Text>
+    <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center' }}>
+    <Carousel
+          layout={"default"}
+          data={dcMovies}
+          sliderWidth={300}
+          itemWidth={300}
+          renderItem={_renderItem}
+           />
+    </View>
+    <Text style={{fontSize: 20, fontWeight: 'bold', paddingBottom: 30, color: '#FFF', marginLeft: 30,  marginTop: 20}}>American</Text>
+    <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center' }}>
+    <Carousel
+          layout={"default"}
+          data={american}
+          sliderWidth={300}
+          itemWidth={300}
+          renderItem={_renderItem}
+           />
+    </View>
+    <Text style={{fontSize: 20, fontWeight: 'bold', paddingBottom: 30, color: '#FFF', marginLeft: 30,  marginTop: 20}}>A copa do mundo é nossa</Text>
+    <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center' }}>
+    <Carousel
+          layout={"default"}
+          data={copa}
+          sliderWidth={300}
+          itemWidth={300}
+          renderItem={_renderItem}
+           />
+    </View>
+    <Text style={{fontSize: 20, fontWeight: 'bold', paddingBottom: 30, color: '#FFF', marginLeft: 30,  marginTop: 20}}>Sessão Ressaca</Text>
+    <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center' }}>
+    <Carousel
+          layout={"default"}
+          data={hangover}
+          sliderWidth={300}
+          itemWidth={300}
+          renderItem={_renderItem}
+           />
+    </View>
+    </ScrollView>
   </SafeAreaView>
     
   );
